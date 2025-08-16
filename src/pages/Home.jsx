95e7b68e1e302fbb3cfc3cf435fb1fd72c16311c
Resmi S <resmi321@gmail.com>
2025-08-16 21:23:36 +0530

    resolved merge conflicts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="p-6 text-center">
       <h1 className="text-3xl font-bold mb-4">Welcome to User Management System</h1>
       <div className="flex justify-center gap-4">
-        <img src="src/Images/System.png" className="w-[1000px] h-[500px]"></img>
+        <img src="https://flowdog.io/wp-content/uploads/2021/11/System-DMS.png" className="w-[1000px] h-[500px]"></img>
         </div>     
          {/* <div className="flex justify-center gap-4">
 
